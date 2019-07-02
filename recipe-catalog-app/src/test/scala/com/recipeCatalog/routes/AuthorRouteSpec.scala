bb632c47b84684c7574f1c90236cc30350318d8d
Samir Benzenine <samir.benzenine@sky.uk>
2019-07-02 12:25:15 +0100

    Updated Author routes tests

--- recipe-catalog-app/src/test/scala/com/recipeCatalog/routes/AuthorRouteSpec.scala
+++ recipe-catalog-app/src/test/scala/com/recipeCatalog/routes/AuthorRouteSpec.scala
@@ -1,13 +1,17 @@
 package com.recipeCatalog.routes
 
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model.{MessageEntity, StatusCodes}
 import com.recipeCatalog.helpers.RouteSpec
 import com.recipeCatalog.model.Author
+import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport._
 import org.bson.types.ObjectId
 import org.mongodb.scala.MongoCollection
 import org.mongodb.scala.model.{InsertOneModel, WriteModel}
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 
-class AuthorRouteSpec extends RouteSpec with BeforeAndAfterAll with BeforeAndAfterEach  {
+class AuthorRouteSpec extends RouteSpec with BeforeAndAfterAll with BeforeAndAfterEach with ScalaFutures with IntegrationPatience {
   val mongo = module.mongo
   val collection: MongoCollection[Author] = mongo.mongoDatabase.getCollection("author")
   val testAuthors = List(
@@ -17,19 +21,27 @@ class AuthorRouteSpec extends RouteSpec with BeforeAndAfterAll with BeforeAndAft
     )
   )
 
+  val authorEntity = Marshal(testAuthors(0)).to[MessageEntity].futureValue
+
   override def beforeAll(): Unit = {
     val writes: List[WriteModel[Author]] = testAuthors.map(item => {
       InsertOneModel(item)
     })
-    collection.bulkWrite(writes)
+    collection.bulkWrite(writes).head().futureValue
 
     super.beforeAll()
   }
 
   "Author API" should {
-    "successfully return authors" in {
+    "successfully get authors" in {
       Get("/api/authors") ~> module.authorRoutes.routes ~> check {
-        responseAs[String] shouldEqual s""""[{"id":"${testAuthors(0)._id}",name:"${testAuthors(0).name}"]"""
+        responseAs[Seq[Author]] should contain allElementsOf testAuthors
+      }
+    }
+
+    "successfully create authors" in {
+      Post("/api/authors").withEntity(authorEntity) ~> module.authorRoutes.routes ~> check {
+        status shouldEqual StatusCodes.OK
       }
     }
   }
