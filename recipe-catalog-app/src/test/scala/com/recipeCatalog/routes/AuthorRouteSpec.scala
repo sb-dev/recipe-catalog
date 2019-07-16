a5303de16ebb4a8c66bb4b0f27ce08c344d813d3
Samir Benzenine <samir.benzenine@sky.uk>
2019-07-16 13:51:18 +0100

    Updated tests for Author routes

--- recipe-catalog-app/src/test/scala/com/recipeCatalog/routes/AuthorRouteSpec.scala
+++ recipe-catalog-app/src/test/scala/com/recipeCatalog/routes/AuthorRouteSpec.scala
@@ -2,7 +2,7 @@ package com.recipeCatalog.routes
 
 import akka.http.scaladsl.marshalling.Marshal
 import akka.http.scaladsl.model.headers.Location
-import akka.http.scaladsl.model.{HttpHeader, MessageEntity, StatusCodes}
+import akka.http.scaladsl.model.{MessageEntity, StatusCodes}
 import com.recipeCatalog.helpers.RouteSpec
 import com.recipeCatalog.model.Author
 import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport._
@@ -25,9 +25,15 @@ class AuthorRouteSpec extends RouteSpec with BeforeAndAfterAll with BeforeAndAft
 
   val testPostAuthor = Author(
     "",
-    "Test Author 1"
+    "Test Author 2"
   )
-  val authorEntity = Marshal(testPostAuthor).to[MessageEntity].futureValue
+  val createAuthorEntity = Marshal(testPostAuthor).to[MessageEntity].futureValue
+
+  val testPutAuthor = Author(
+    "5d16314256ffa4012e827b45",
+    "Test Author 1.2"
+  )
+  val updateAuthorEntity = Marshal(testPutAuthor).to[MessageEntity].futureValue
 
   override def beforeAll(): Unit = {
     val writes: List[WriteModel[Author]] = testAuthors.map(item => {
@@ -42,18 +48,56 @@ class AuthorRouteSpec extends RouteSpec with BeforeAndAfterAll with BeforeAndAft
     "successfully get authors" in {
       Get("/api/authors") ~> module.authorRoutes.routes ~> check {
         responseAs[Seq[Author]] should contain allElementsOf testAuthors
+        responseAs[Seq[Author]] should have size (1)
       }
     }
 
     "successfully create authors" in {
-      Post("/api/authors").withEntity(authorEntity) ~> module.authorRoutes.routes ~> check {
+      Post("/api/authors").withEntity(createAuthorEntity) ~> module.authorRoutes.routes ~> check {
         status shouldEqual StatusCodes.Created
         header("Location") shouldEqual Some(Location(s"api/author/${idGenerator.generate}"))
       }
     }
 
     "successfully get authors by Id" in {
-      Get("/api/authors/5d16314256ffa4012e827b45") ~> module.authorRoutes.routes ~> check {
+      Get(s"/api/authors/${testAuthors(0)._id}") ~> module.authorRoutes.routes ~> check {
+        status shouldEqual StatusCodes.OK
+        responseAs[Author] shouldEqual testAuthors(0)
+      }
+    }
+
+    "Fail with 404 when retrieving an author with unknown Id" in {
+      Get(s"/api/authors/unknown_id") ~> module.authorRoutes.routes ~> check {
+        status shouldEqual StatusCodes.NotFound
+      }
+    }
+
+    "successfully update an author" in {
+      Put(s"/api/authors/${testPutAuthor._id}").withEntity(updateAuthorEntity) ~> module.authorRoutes.routes ~> check {
+        status shouldEqual StatusCodes.Created
+      }
+    }
+
+    "successfully delete an author" in {
+      Delete(s"/api/authors/${idGenerator.generate}") ~> module.authorRoutes.routes ~> check {
+        status shouldEqual StatusCodes.OK
+      }
+    }
+
+    "Fail silently when deleting an unknown author" in {
+      Delete("/api/authors/unknown_id") ~> module.authorRoutes.routes ~> check {
+        status shouldEqual StatusCodes.OK
+      }
+    }
+
+    "successfully create a recipe" in {
+      Delete(s"/api/authors/${idGenerator.generate}") ~> module.authorRoutes.routes ~> check {
+        status shouldEqual StatusCodes.OK
+      }
+    }
+
+    "successfully get recipes by author Id" in {
+      Delete(s"/api/authors/5d16314256ffa4012e827b45/recipes") ~> module.authorRoutes.routes ~> check {
         status shouldEqual StatusCodes.OK
       }
     }
